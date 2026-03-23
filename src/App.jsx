504a75ed6682962c54bf9c0eb1a806516ccb4395
Jude Andersen <jude.ottilie@gmail.com>
2026-03-23 14:21:02 -0500

    update

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+export const App = () => {  
+  const [count, setCount] = useState(0) // [stateVariable, setterFunction]
+
+  const handleButtonClick = () => {
+    setCount(count + 1)
+    console.log(count)
+  }
+  
+  return (
+    <>
+      <h1>Hello!</h1>
+      <div>This is amazing!</div>
+      <button className="btn-secondary" onClick={handleButtonClick}>
+        Click me!
+      </button>
+      <div>Count: {count}</div>
+    </>
+  )
+}
