03950a478b4c003acd1ab6d63d8e72b322290193
Jude Andersen <jude.ottilie@gmail.com>
2026-03-25 09:31:39 -0500

    dave debugged, page shows up now with additions

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import { useEffect, useState } from "react"
-import { getAllTickets } from "./services/ticketServices"
+import { getAllTickets } from "./services/ticketService"
+import "./App.css"
 
 export const App = () => {
   const [allTickets, setAllTickets] = useState([])
 
   useEffect(() => {
- getAllTickets().then((ticketsArray) => {
-    setAllTickets(ticketsArray)
-    console.log("tickets set!")
-   })
+    getAllTickets().then((ticketsArray) => {
+      setAllTickets(ticketsArray)
+      console.log("tickets set!")
+    })
 
   }, []) // ONLY runs on initial render of component
 
-    return <></>
-
+    return <div className="tickets-container">
+        <h2>Tickets</h2>
+        <article className="tickets">
+          {allTickets.map(ticket => {
+            return (
+              <section className="ticket" key= {ticket.id}>
+                <header className="ticket-info">#{ticket.id}</header>
+                <div>{ticket.description}</div>
+                <footer>
+                  <div>
+                    <div className="ticket-info">emergency</div>
+                      <div>{ticket.emergency ? "yes" : "no"}</div>
+                  </div>
+                </footer>
+              </section>
+            )
+          })}
+        </article>
+      </div>
 }
