9fac6fcb898cbb858caf5c028a8e450ce0f35441
Jude Andersen <jude.ottilie@gmail.com>
2026-03-30 14:32:23 -0500

    p2 c1

--- src/components/tickets/Ticket.jsx
+++ src/components/tickets/Ticket.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react"
+import { getAllEmployees } from "../../services/employeeService.jsx"
+
 export const Ticket = ({ ticket, name }) => {
+    const [employees, setEmployees] = useState([])
+    const [assignedEmployee, setAssignedEmployee] = useState({})
+
+    useEffect (() => {
+        getAllEmployees().then((employeesArray) => {
+            setEmployees(employeesArray)
+        })
+}, []) //having the empty dependency array makes the function stop after the initial render
+
+    useEffect(() => {
+      const foundEmployee = employees.find(
+        (employee) => employee.id === ticket.employeeTickets[0]?.employeeId
+      )
+      setAssignedEmployee(foundEmployee)
+    }, [employees, ticket])
+
   return (
     <section className="ticket" key={ticket.id}>
       <header className="ticket-info">#{ticket.id}</header>
       <div>{ticket.description}</div>
       <footer>
+        <div>
+            <div className="ticket-info">assignee</div>
+            <div>
+                {assignedEmployee ? assignedEmployee.user?.fullName : "None"}
+            </div>
+        </div>
         <div>
           <div className="ticket-info">emergency</div>
           <div>{ticket.emergency ? "yes" : "no"}</div>
